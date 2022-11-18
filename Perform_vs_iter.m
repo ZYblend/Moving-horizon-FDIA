071ea0c038bac18cd6ea6b72ce2918ce9eb614f6
ZYblend <36635562+ZYblend@users.noreply.github.com>
2022-11-17 19:46:43 -0500

    finalize simulation on 14-bus system

--- Perform_vs_iter.m
+++ Perform_vs_iter.m
@@ -4,26 +4,23 @@ close all
 clc
 
 %% simulation parameters
-tot = 2000;
+tot = 20000;
 num_iter = 50;
 Max_iter = round(linspace(100,tot,num_iter));  % iterations of PGA algorithm
 run_model
+% 
+% model = 'System_model_discrete';
+% load_system(model);
+% Simulink.BlockDiagram.buildRapidAcceleratorTarget(model);
 
 
 %% Sim
-mean_effect_MHFDIA = zeros(num_iter,1);
-max_effect_MHFDIA  = zeros(num_iter,1);
-min_effect_MHFDIA  = zeros(num_iter,1);
+Mean_effect_MHFDIA = zeros(num_iter,1);
+Max_effect_MHFDIA  = zeros(num_iter,1);
+Min_effect_MHFDIA  = zeros(num_iter,1);
 Max_BDD_MHFDIA     = zeros(num_iter,1);
 Mean_BDD_MHFDIA    = zeros(num_iter,1);
 Min_BDD_MHFDIA     = zeros(num_iter,1);
-
-mean_effect_EIGFDIA = zeros(num_iter,1);
-max_effect_EIGFDIA  = zeros(num_iter,1);
-min_effect_EIGFDIA  = zeros(num_iter,1);
-Max_BDD_EIGFDIA     = zeros(num_iter,1);
-Mean_BDD_EIGFDIA    = zeros(num_iter,1);
-Min_BDD_EIGFDIA     = zeros(num_iter,1);
 
 N_start_attack = 0.2*N_samples+T;   % start attack injection
 
@@ -35,57 +32,37 @@ for idx = 1:num_iter
     % State vectors
     x           = simOut.logsout.getElement('x').Values.Data; % true states
     x_hat_L2O   = simOut.logsout.getElement('x_hat_L2O').Values.Data;  % Observed states (MHFDIA)
-    x_hat_L2O2  = simOut.logsout.getElement('x_hat_L2O2').Values.Data;  % Observed states (MHFDIA)
+%     x_hat_L2O2  = simOut.logsout.getElement('x_hat_L2O2').Values.Data;  % Observed states (eig FDIA)
 
     BDD_res1   = simOut.logsout.getElement('BDD_res1').Values.Data; % Bad data residue(MHFDIA)
-    BDD_res2   = simOut.logsout.getElement('BDD_res2').Values.Data; % Bad data residue (eig FDIA)
+%     BDD_res2   = simOut.logsout.getElement('BDD_res2').Values.Data; % Bad data residue (eig FDIA)
     
     % effectiveness
-    effect1 = vecnorm(x - x_hat_L2O,2,2)./vecnorm(x,2,2);
+    effect1 = vecnorm(x - x_hat_L2O,2,2);
     effect1(1:N_start_attack) = 0;
-    effect2 = vecnorm(x - x_hat_L2O2,2,2)./vecnorm(x,2,2);
-    effect2(1:N_start_attack) = 0;
+%     effect2 = vecnorm(x - x_hat_L2O2,2,2);
+%     effect2(1:N_start_attack) = 0;
     
     % BDD Residual
-    BDD_res2(1:N_start_attack)=0;
+%     BDD_res2(1:N_start_attack)=0;
     BDD_res1(1:N_start_attack)=0;
     
     % MH_FDIA
-    mean_effect_MHFDIA(idx) = mean(effect1);
-    max_effect_MHFDIA(idx) = max(effect1);
-    min_effect_MHFDIA(idx) = min(effect1);
+    Mean_effect_MHFDIA(idx) = mean(effect1);
+    Max_effect_MHFDIA(idx) = max(effect1);
+    Min_effect_MHFDIA(idx) = min(effect1);
 
     Max_BDD_MHFDIA(idx) = max(BDD_res1);
     Mean_BDD_MHFDIA(idx) = mean(BDD_res1);
     Min_BDD_MHFDIA(idx) = min(BDD_res1);
-    
-    % Eig_FDIA
-    mean_effect_EIGFDIA(idx) = mean(effect2);
-    max_effect_EIGFDIA(idx) = max(effect2);
-    min_effect_EIGFDIA(idx) = min(effect2);
-    
-    Max_BDD_EIGFDIA(idx) = max(BDD_res2);
-    Mean_BDD_EIGFDIA(idx) = mean(BDD_res2);
-    Min_BDD_EIGFDIA(idx) = min(BDD_res2);   
+     
 end
 
-save mean_effect_MHFDIA.mat mean_effect_MHFDIA
-save max_effect_MHFDIA.mat max_effect_MHFDIA
-save min_effect_MHFDIA.mat min_effect_MHFDIA
+save('sim_out/effect.mat', 'Mean_effect_MHFDIA', 'Max_effect_MHFDIA', 'Min_effect_MHFDIA', '-v7.3')
 
-save Max_BDD_MHFDIA.mat Max_BDD_MHFDIA
-save Mean_BDD_MHFDIA.mat Mean_BDD_MHFDIA
-save Min_BDD_MHFDIA.mat Min_BDD_MHFDIA
+save('sim_out/stealth.mat', 'Mean_BDD_MHFDIA', 'Max_BDD_MHFDIA', 'Min_BDD_MHFDIA', '-v7.3')
 
-save mean_effect_EIGFDIA.mat mean_effect_EIGFDIA
-save max_effect_EIGFDIA.mat max_effect_EIGFDIA
-save min_effect_EIGFDIA.mat min_effect_EIGFDIA
-
-save Max_BDD_EIGFDIA.mat Max_BDD_EIGFDIA
-save Mean_BDD_EIGFDIA.mat Mean_BDD_EIGFDIA
-save Min_BDD_EIGFDIA.mat Min_BDD_EIGFDIA
-
-save Max_Iter.mat Max_iter
+save sim_out/Max_Iter.mat Max_iter
 
     
     
--- plot_perform_vs_iter.m
+++ plot_perform_vs_iter.m
@@ -2,28 +2,22 @@
 
 
 %% load data
-load mean_effect_MHFDIA.mat
-load max_effect_MHFDIA.mat
-load min_effect_MHFDIA.mat
+% load mean_effect_MHFDIA.mat
+% load max_effect_MHFDIA.mat
+% load min_effect_MHFDIA.mat
+% 
+% load Max_BDD_MHFDIA.mat
+% load Mean_BDD_MHFDIA.mat
+% load Min_BDD_MHFDIA.mat
 
-load Max_BDD_MHFDIA.mat
-load Mean_BDD_MHFDIA.mat
-load Min_BDD_MHFDIA.mat
+run_model;
 
-load mean_effect_EIGFDIA.mat
-load max_effect_EIGFDIA.mat
-load min_effect_EIGFDIA.mat
+load('sim_out/effect.mat');
+load('sim_out/stealth.mat');
 
-load Max_BDD_EIGFDIA.mat
-load Mean_BDD_EIGFDIA.mat
-load Min_BDD_EIGFDIA.mat
-
-load Max_Iter.mat
+load sim_out/Max_Iter.mat;
 
 Max_Iter2 = [Max_iter, fliplr(Max_iter)];
-BDD_thresh = 0.05;
-
-T = 20;
 
 LW = 1.5;  % linewidth
 FS = 15;   % font size
@@ -31,21 +25,21 @@ FS = 15;   % font size
 %% plotting
 % MH-FDIA 
 figure,
-subplot(2,2,1)
-plot(Max_iter,mean_effect_MHFDIA,'k','LineWidth',LW)
+subplot(1,2,1)
+plot(Max_iter,Mean_effect_MHFDIA,'k','LineWidth',LW)
 ylabel('Eeffectiveness','FontWeight','bold')
-title('MH-FDIA (proposed)');
+xlabel('Maximum iteration allowance')
 set(gca,'fontweight','bold','fontsize',12) 
 set(gca,'LineWidth',LW)
 
-inbetween_yc = [max_effect_MHFDIA.', fliplr(min_effect_MHFDIA.')];
+inbetween_yc = [Max_effect_MHFDIA.', fliplr(Min_effect_MHFDIA.')];
 hold on, h1 = fill(Max_Iter2,inbetween_yc,'k');
 set(h1,'facealpha',.3)
 
-subplot(2,2,3)
+subplot(1,2,2)
 plot(Max_iter,Mean_BDD_MHFDIA,'k','LineWidth',LW)
 ylabel('Stealthiness','FontWeight','bold')
-xlabel('time')
+xlabel('Maximum iteration allowance')
 set(gca,'fontweight','bold','fontsize',12) 
 set(gca,'LineWidth',LW)
 
@@ -54,28 +48,28 @@ hold on, h2 = fill(Max_Iter2,inbetween_r,'k');
 set(h2,'facealpha',.3)
 
 hold on;
-plot(Max_iter,0.9*T*BDD_thresh*ones(length(Max_iter),1),'r--','LineWidth',2*LW)
+plot(Max_iter,T*BDD_thresh*ones(length(Max_iter),1),'r--','LineWidth',2*LW)
 
-% Eig-FDIA 
-subplot(2,2,2)
-plot(Max_iter,mean_effect_EIGFDIA,'k','LineWidth',LW)
-title('FDIA (literature)');
-set(gca,'fontweight','bold','fontsize',12) 
-set(gca,'LineWidth',LW)
-
-inbetween_yc = [max_effect_EIGFDIA.', fliplr(min_effect_MHFDIA.')];
-hold on, h1 = fill(Max_Iter2,inbetween_yc,'k');
-set(h1,'facealpha',.3)
-
-subplot(2,2,4)
-plot(Max_iter,Mean_BDD_EIGFDIA,'k','LineWidth',LW)
-xlabel('time')
-set(gca,'fontweight','bold','fontsize',12) 
-set(gca,'LineWidth',LW)
-
-inbetween_r = [Max_BDD_EIGFDIA.', fliplr(Min_BDD_EIGFDIA.')];
-hold on, h2 = fill(Max_Iter2,inbetween_r,'k');
-set(h2,'facealpha',.3)
-
-hold on;
-plot(Max_iter,0.9*T*BDD_thresh*ones(length(Max_iter),1),'r--','LineWidth',2*LW)
+% % Eig-FDIA 
+% subplot(2,2,2)
+% plot(Max_iter,mean_effect_EIGFDIA,'k','LineWidth',LW)
+% title('FDIA (literature)');
+% set(gca,'fontweight','bold','fontsize',12) 
+% set(gca,'LineWidth',LW)
+% 
+% inbetween_yc = [max_effect_EIGFDIA.', fliplr(min_effect_MHFDIA.')];
+% hold on, h1 = fill(Max_Iter2,inbetween_yc,'k');
+% set(h1,'facealpha',.3)
+% 
+% subplot(2,2,4)
+% plot(Max_iter,Mean_BDD_EIGFDIA,'k','LineWidth',LW)
+% xlabel('time')
+% set(gca,'fontweight','bold','fontsize',12) 
+% set(gca,'LineWidth',LW)
+% 
+% inbetween_r = [Max_BDD_EIGFDIA.', fliplr(Min_BDD_EIGFDIA.')];
+% hold on, h2 = fill(Max_Iter2,inbetween_r,'k');
+% set(h2,'facealpha',.3)
+% 
+% hold on;
+% plot(Max_iter,0.9*T*BDD_thresh*ones(length(Max_iter),1),'r--','LineWidth',2*LW)
