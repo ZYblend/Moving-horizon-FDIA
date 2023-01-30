d8b42021932f2f32b73665a2d2c3414ae032016e
ZYblend <36635562+ZYblend@users.noreply.github.com>
2023-01-30 15:51:35 -0500

    add attacks to kinematic control case

--- kinemati_control/Run_model.m
+++ kinemati_control/Run_model.m
@@ -4,6 +4,8 @@ clc
 
 %% parameter
 d  = 0.1;                        % distance betwen medium point of axis of wheels and center of mass(m)
+L=0.235/2;  %1/2 distance between two wheels,(m)
+r=0.05;     %radius of wheels,(m)
 
 
 %% Initial condition
@@ -29,51 +31,6 @@ T_final = 500;
 kx = 15;
 ky = 15;
 
-%% MPC Controller
-L = 10;  % horizon
-R = 0.1*eye(n_int);
-Q = 5*eye(n_states);  
-P = 5*eye(n_states); 
-P_alpha = 0.01*eye(n_int);
-
-u_up = [0.7; 1.5];
-u_lo = [-0.7; -1.5];
-
-rate_bound = 0.2;
-
-D_bar = @(N,n)[zeros((N-1)*n,n), eye((N-1)*n)];
-D_under = @(N,n)[eye((N-1)*n),zeros((N-1)*n,n)];
-E = kron([1, zeros(1,L)],eye(n_states));
-% E2 = kron([zeros(1,L),1],diag([0 1 1]));
-% 
-% E2_up = [0; 1; 2];
-% E2_lo = [0; -1; 0];
-
-A = @(z) [1 0 0;
-     -Ts*sin(z(1))-d*Ts*cos(z(1)) 1 0;
-     Ts*cos(z(1))-d*Ts*sin(z(1)) 0 1];
-B = @(z) Ts*[0 1;
-          cos(z(1)) -d*sin(z(1));
-          sin(z(1)) d*cos(z(1))];
-
-H1 = blkdiag(kron(eye(L),Q), P);
-H2 = kron(eye(L),R);
-H3 = kron(eye(L),P_alpha);
-H = blkdiag(H1,H2,H3);
-
-Aeq = @(z) [D_bar(L+1,n_states)-kron(eye(L),A(z))*D_under(L+1,n_states) -kron(eye(L),B(z)) zeros(L*n_states,n_int*L);
-            E zeros(n_states,n_int*L) zeros(n_states,n_int*L)];
-
-Aineq = [zeros(n_int*L,n_states*(L+1)) eye(n_int*L) -eye(n_int*L);
-         zeros(n_int*L,n_states*(L+1)) -eye(n_int*L) -eye(n_int*L);
-         zeros(n_int*(L-1),n_states*(L+1)) D_bar(L,n_int)-D_under(L,n_int) zeros(n_int*(L-1),n_int*L);
-         zeros(n_int*(L-1),n_states*(L+1)) D_under(L,n_int)-D_bar(L,n_int) zeros(n_int*(L-1),n_int*L)];
-bineq = [kron(ones(L,1),u_up);
-         kron(ones(L,1),u_up);
-         rate_bound*ones(n_int*(L-1),1);
-         rate_bound*ones(n_int*(L-1),1)];
-
-
 
 %% covariance of measurement noise
 R_meas = 1e-4;
@@ -84,4 +41,67 @@ P_diag = P_inp * eye(n_states);
 
 
 %% lineraization in T horizon 
+Ad = eye(n_states);
+Bd = @(theta) [0 1;
+               cos(theta) -d*sin(theta);
+               sin(theta) d*cos(theta)] * Ts;
+% C = @(x,y,theta) [-x*sin(theta) cos(theta) 0;
+%                    y*cos(theta) 0 sin(theta);
+%                    0 1 0;
+%                    0 0 1;
+%                    0 1/(4*r) L/(4*r);
+%                    0 1/(4*r) -L/(4*r)];
+% Cd = C(0,0,1.5);
 
+
+%% Attack Parameters
+pa = 0.4;
+n_attack =  round(pa*n_meas);
+T_start_attack = .2*T_final;  % Time to begin attack
+I_single = sort(randperm(n_meas,n_attack)).';
+
+
+%% Bad Data Detection
+BDD_thresh = 1;  % Bad data detection tolerance
+
+% %% L2 moving-horizon attack generation
+% T = 10;
+% % initial T attacks (not injected in system, only for atatck design)
+% T1 = 2*T;   % time horizon for FDIA design
+% [PhiT1,HT1,Theta_T1,G_T1] = opti_params(Ad,Bd,Cd,T1);
+% 
+% I_single = sort(randperm(n_meas,n_attack)).';
+% % I_single = [1;2;9;11;12;16;17];
+% I_attack_ini = repmat(I_single,1,T1);           % fixed attack support
+% 
+% % flat attack support for T horizon
+% I_aux = linspace(0,(T1-1)*n_meas,T1);
+% I_aux = repmat(I_aux,n_attack,1);
+% I_attack = I_attack_ini+I_aux;
+% I_attack = I_attack(:);
+% 
+% % initial T attacks
+% e_ini = L2_FDIA(PhiT1,I_attack(:),0.8*T1*BDD_thresh);
+% e_ini_matrix = reshape(e_ini,n_meas,T1);
+% 
+% % moving-horizon FDIA parameters
+% [U,S,V] = svd(PhiT1);
+% U_history = U(1:(T1-1)*n_meas,:);
+% U_i = U((T1-1)*n_meas+1:end,:);
+% S1 = S(1:n_states,1:n_states);
+% 
+% IO = [eye(n_states) zeros(n_states,T1*n_meas-n_states)];
+% OI = [zeros(T1*n_meas-n_states,n_states) eye(T1*n_meas-n_states)];
+% 
+% A1 = inv(S1)*IO*U_i.';
+% A2 = OI*U_i.';
+% b1p = inv(S1)*IO*U_history.';
+% b2p = OI*U_history.';
+% 
+max_iter = 2000;   % maximal number of iteration of PGA algorithm
+% 
+% 
+% %% L2 moving-horizon FDIA attack generation (Eigenvalue problem version)
+% Se1 = S(1:n_states,1:n_states);
+% Ue11 = U(I_attack(1:(T1-1)*n_attack),1:n_states);
+% Ue12 = U(I_attack((T1-1)*n_attack+1:end),1:n_states);
