839d1ee3648a9591a9f6fd4e79c8dc0a80b31615
ZYblend <zyby170412@gmail.com>
2023-01-31 17:44:46 -0500

    add dataset

--- kinemati_control/parameter_estimation/Run_model.m
+++ kinemati_control/parameter_estimation/Run_model.m
@@ -9,12 +9,20 @@ d  = 0.1;                        % distance betwen medium point of axis of wheel
 Ts = 0.01;
 
 %% dataset
-theta = load('dataset/data1').theta;
-x     = load('dataset/data1').x;
-y     = load('dataset/data1').y;
+theta = load('dataset/dataset1').theta;
+x     = load('dataset/dataset1').x;
+y     = load('dataset/dataset1').y;
 
-v     = load('dataset/data1').v;
-w     = load('dataset/data1').w;
+v     = load('dataset/dataset1').v;
+w     = load('dataset/dataset1').w;
+
+time_serise = linspace(0,round(Ts*size(theta,1)),size(theta,1)).';
+
+theta_time = [time_serise, theta];
+x_time =  [time_serise, x];
+y_time =  [time_serise, y];
+v_time =  [time_serise, v];
+w_time =  [time_serise, w];
 
 %% initial condition
 z_initial = [theta(1); x(1); y(1)];
